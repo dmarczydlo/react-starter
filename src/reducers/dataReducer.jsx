4b71f3a89fb1ddeeabb9167211ad33805a6c1254
Daniel Marczydlo <daniel.marczydlo@gmail.com>
2017-11-23 18:17:47 +0100

    eslint config udpdated, babel async await added

--- src/reducers/dataReducer.jsx
+++ src/reducers/dataReducer.jsx
@@ -1,11 +1,9 @@
-const initialState = {
-
-};
+const initialState = {};
 
 const dataReducer = (state = initialState, action) => {
     switch (action.type) {
-    default:
-        return state;
+        default:
+            return state;
     }
 };
 
