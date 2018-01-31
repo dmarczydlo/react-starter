94a02b7ea1742c0ec2034f581b1750d35d728989
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-01-31 19:48:29 +0100

    fix lint

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import {createStore, applyMiddleware} from 'redux';
-import {install} from 'offline-plugin/runtime'
+import {install} from 'offline-plugin/runtime';
 import {Provider} from 'react-redux';
 import reducers from './reducers';
 import App from './sites/app';
@@ -11,6 +11,6 @@ const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     install();
 }
