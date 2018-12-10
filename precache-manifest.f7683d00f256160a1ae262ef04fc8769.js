179edd835c16451e25ff126d7f8b2a17047ef59a
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-12-10 18:38:56 +0100

    Updates

---
+++ precache-manifest.f7683d00f256160a1ae262ef04fc8769.js
@@ -0,0 +1,16 @@
+self.__precacheManifest = [
+  {
+    "url": "/style.154a1b850d3d836d432a.css"
+  },
+  {
+    "revision": "72ba534633542168691ab1be8dd3dcbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d3414e70cf0f54245819ead71c8cc32",
+    "url": "/icon.png"
+  },
+  {
+    "url": "/154a1b850d3d836d432a.js"
+  }
+];
