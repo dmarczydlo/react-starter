083a9314464e504a4d9632e7818b04b84e484995
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-12-24 12:38:28 +0100

    update scripts

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,5 +1,59 @@
 #!/usr/bin/env bash
 
+
+removeTestAndExamples()
+{
+ echo "Remove unnecessary files"
+ #remove test directory
+ rm -rf test
+ rm -rf cypress
+ rm cypress.json
+ rm .travis.yml
+ rm _config.yml
+ rm package-lock.json
+ #remove unnecessary dependencies
+ yarn remove jest --silent && yarn remove --dev  babel-jest coveralls cypress enzyme enzyme-adapter-react-16 enzyme-to-json fast-check fetch-mock node-fetch sinon --silent
+ #remove scripts
+ tmp=$(mktemp)
+ SCRIPTS='{
+          "start": "webpack-dev-server --mode development --progress --profile --colors --open",
+          "build": "webpack --mode production --config=webpack.production.config.js --progress --profile --colors"
+        }'
+
+jq --argjson v "$SCRIPTS"  '.scripts = $v' package.json > "$tmp" && mv "$tmp" package.json 
+
+jq 'del(.directories)' package.json > "$tmp" && mv "$tmp" package.json
+jq 'del(.jest)' package.json > "$tmp" && mv "$tmp" package.json
+ #remove examples (tbd)
+}
+
+echo "Please select option for start:"
+PS3='Your option: '
+options=("Empty project" "Full project (with tests and examples component" "Quit");
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Empty project")
+            echo "you chose choice 1"
+            break
+            ;;
+        "Full project (with tests and examples component")
+            echo "you chose choice 2"
+            break
+            ;;
+
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+if [ "$opt" == "Quit" ]; then
+  echo "The script has been interrupted"
+  exit 1
+fi
+
 echo "Please enter project name"
 read PROJECT_NAME
 
@@ -20,14 +74,18 @@ if ! [ -x "$(command -v jq)" ]; then
     sudo apt-get install jq
 fi
 
+if [ "$opt" == "Empty project" ]; then
+  removeTestAndExamples
+fi
+
 tmp=$(mktemp)
 jq --arg v "$PROJECT_NAME"  '.name = $v' package.json > "$tmp" && mv "$tmp" package.json
 
 
 if [ -x "$(command -v yarn)" ]; then
-    yarn
+    yarn --silent
 else
-npm i
+    npm i --silent
 fi
 
 if [ -x "$(command -v git)" ]; then
